d72c9411ac46d14f388a6f926049b20df93a4d50
James Douglass <jamesdouglassusa@gmail.com>
2015-04-15 10:05:48 -0700

    Writing options to opt file, dl GEOS

--- build.py
+++ build.py
@@ -5,13 +5,35 @@ import urllib2
 import time
 import shutil
 import subprocess
+import zipfile
+import zlib
+import platform
 
-def download_gdal(version):
+def _download_archive(url):
+    out_file = os.path.basename(url)
+    u = urllib2.urlopen(url)
+    local_file = open(out_file, 'wb')
+    local_file.write(u.read())
+    local_file.close()
+
+def download_geos(version):
+    # parse out the version info
+    major, minor, release = version_info(version)
+    geos_download_uri = 'http://download.osgeo.org/geos/'
+    if minor >= 10:
+        geos_download_uri += version = '/'
+
+    local_gzip = 'geos=%s.tar.bz2' % version
+    geos_download_uri += local_gzip
+    _download_archive(geos_download_uri)
+
+def download_gdal(version, fmt='gz'):
     """Download the approprate version of the GDAL source from the official
     download mirror.
 
     version - the version of GDAL to download.  Must be a string in the form
         1.1.1
+    fmt - the archive format to download.  One of 'gz'|'zip'
 
     Returns the absolute path to the downloaded tar.gz file."""
 
@@ -22,18 +44,34 @@ def download_gdal(version):
     if minor >= 10:
         gdal_download_uri += version + '/'
 
-    local_gzip = 'gdal-%s.tar.gz' % version
+    if fmt == 'gz':
+        local_gzip = 'gdal-%s.tar.gz' % version
+    elif fmt == 'zip':
+        local_gzip = 'gdal%s.zip' % version.replace('.', '')
+    else:
+        raise Exception('format %s not recognized!' % fmt)
     gdal_download_uri += local_gzip
     print gdal_download_uri
 
     print 'downloading ...'
-    u = urllib2.urlopen(gdal_download_uri)
-    localFile = open(local_gzip, 'w')
-    localFile.write(u.read())
-    localFile.close()
+    _download_archive(gdal_download_uri)
 
     return os.path.abspath(local_gzip)
 
+def set_gdal_home(filepath):
+    tmp_out_filepath = os.path.join(os.getcwd(), 'tmp_%s' %
+        os.path.basename(filepath))
+    new_out_file = open(tmp_out_filepath, 'w')
+    new_gdal_home = os.path.join(os.path.dirname(__file__), 'build')
+    with open(filepath, 'r') as old_file:
+        for line in old_file:
+            if line.startswith('GDAL_HOME'):
+                new_out_file.write('GDAL_HOME = "%s"\n' % new_gdal_home)
+            elif line.startswith('PYDIR'):
+                new_out_file.write('PYDIR = "%s"\n' %
+                    os.path.dirname(sys.executable))
+            else:
+                new_out_file.write(line)
 
 if __name__ == '__main__':
     start_time = time.time()
